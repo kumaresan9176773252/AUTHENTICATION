c4918e8ae7ef0a7dcaae35244330159191735311
kumaresan9176773252 <kumara2275012@gmail.com>
2024-01-08 11:14:14 +0530

    first commit

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,82 @@
+import { Button, TextField, Typography } from '@mui/material'
+import React, { useState } from 'react'
+import { Box } from '@mui/material';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { authActions } from '../store';
+
+const Login = () => {
+    const dispatch = useDispatch();
+    const history = useNavigate();
+    const [inputs, setinputs] = useState({
+        email: '',
+        password: ''
+    });
+    const handleChange = (e) => {
+        setinputs(perv => ({
+            ...perv,
+            [e.target.name]: e.target.value
+        }));
+    }
+
+    const sendRequest = async () => {
+        const res = await axios.post('http://localhost:5000/api/login', {
+            email: inputs.email,
+            password: inputs.password
+        })
+            .catch(err => console.log(err));
+        const data = await res.data;
+        return data;
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        sendRequest()
+            .then(() => dispatch(authActions.login()))
+            .then(() => history("/user"));
+        // send http request 
+    }
+    return (
+        <div>
+
+            <form onSubmit={handleSubmit}>
+                <Box
+                    marginLeft={'auto'}
+                    marginRight={'auto'}
+                    width={300}
+                    display={'flex'}
+                    flexDirection={'column'}
+                    justifyContent={'center'}
+                    alignItems={'center'}
+                >
+                    <Typography variant='h2'>Login</Typography>
+
+
+                    <TextField
+                        name='email'
+                        onChange={handleChange}
+                        type='email'
+                        value={inputs.email}
+                        variant='outlined'
+                        placeholder='Email'
+                        margin='normal'
+                    />
+                    <TextField
+                        name='password'
+                        onChange={handleChange}
+                        type='password'
+                        value={inputs.password}
+                        variant='outlined'
+                        placeholder='Password'
+                        margin='normal'
+                    />
+                    <Button variant='contained' type='submit'>Login</Button>
+                </Box>
+            </form>
+        </div>
+    )
+
+};
+
+export default Login;
---
+++ frontend/src/components/Signup.js
@@ -0,0 +1,83 @@
+import { Button, TextField, Typography } from '@mui/material'
+import React, { useState } from 'react'
+import { Box } from '@mui/material';
+import  axios  from 'axios';
+import { useNavigate } from 'react-router-dom';
+const Signup = () => {
+    const history = useNavigate();
+    const [inputs, setinputs] = useState({
+        name: '',
+        email: '',
+        password: ''
+    });
+    const handleChange = (e) => {
+        setinputs(perv => ({
+            ...perv,
+            [e.target.name]: e.target.value
+        }));
+    }
+
+    const sendRequest = async () => {
+        const res = await axios.post('http://localhost:5000/api/signup', {
+            name: inputs.name,
+            email: inputs.email,
+            password: inputs.password
+        }).catch(err => console.log(err));
+        const data = await res.data;
+        return data;
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        sendRequest().then(() => history("/login"));
+        // send http request 
+    }
+    return (
+        <div>
+
+            <form onSubmit={handleSubmit}>
+                <Box
+                    marginLeft={'auto'}
+                    marginRight={'auto'}
+                    width={300}
+                    display={'flex'}
+                    flexDirection={'column'}
+                    justifyContent={'center'}
+                    alignItems={'center'}
+                >
+                    <Typography variant='h2'>Signup</Typography>
+
+                    <TextField
+                        name='name'
+                        onChange={handleChange}
+                        value={inputs.name}
+                        variant='outlined'
+                        placeholder='Name'
+                        margin='normal'
+                    />
+                    <TextField
+                        name='email'
+                        onChange={handleChange}
+                        type='email'
+                        value={inputs.email}
+                        variant='outlined'
+                        placeholder='Email'
+                        margin='normal'
+                    />
+                    <TextField
+                        name='password'
+                        onChange={handleChange}
+                        type='password'
+                        value={inputs.password}
+                        variant='outlined'
+                        placeholder='Password'
+                        margin='normal'
+                    />
+                    <Button variant='contained' type='submit'>Signup</Button>
+                </Box>
+            </form>
+        </div>
+    )
+};
+
+export default Signup;
